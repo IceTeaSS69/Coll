17a275b10d48f8796a22c5c6aaff50fccbdaa43a
IceTeaSS69 <dima.kuzin.1010@gmail.com>
2025-05-10 20:51:16 +0300

    Done 1 task

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int a = 5;
+int b = 6;
+int c = a + b;
+if (c % 2 == 0)
+{
+    Console.WriteLine("Число Чётное");
+}
+else
+{
+    Console.WriteLine("Число НЕчётное");
+}
+    Console.WriteLine(a + b); //Первая задача
